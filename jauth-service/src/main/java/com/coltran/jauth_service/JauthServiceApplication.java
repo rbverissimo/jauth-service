89af72c19fab3b1708d1a9e2d0f56dbb1805ed58
Renato Verissimo <rverissimo.dev@gmail.com>
2025-10-15 17:15:02 -0300

    fix: remove forwardPort instruction and a SpringBoot configuration to ignore the datasource for now

--- jauth-service/src/main/java/com/coltran/jauth_service/JauthServiceApplication.java
+++ jauth-service/src/main/java/com/coltran/jauth_service/JauthServiceApplication.java
@@ -2,8 +2,9 @@ package com.coltran.jauth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JauthServiceApplication {
 
 	public static void main(String[] args) {
