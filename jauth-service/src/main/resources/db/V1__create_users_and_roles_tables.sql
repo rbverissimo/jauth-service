781681aedd53a4b3381add7198b93e7ef56ea1ba
Renato Verissimo <rverissimo.dev@gmail.com>
2025-10-21 14:08:09 -0300

    feat: add first migration run

--- jauth-service/src/main/resources/db/V1__create_users_and_roles_tables.sql
+++ jauth-service/src/main/resources/db/V1__create_users_and_roles_tables.sql
@@ -7,6 +7,18 @@ CREATE TABLE roles (
 
 CREATE TABLE users (
     id ULID PRIMARY KEY DEFAULT ulid(),
-    username VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    email VARCHAR(100) NOT NULL UNIQUE,
+    public_name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    verified_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE users_roles (
+    user_id ULID NOT NULL,
+    role_id ULID NOT NULL,
+    PRIMARY KEY(user_id, role_id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id)
 );
