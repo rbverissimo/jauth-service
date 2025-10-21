653f146443668a081c46dbe5953c914f7c3d5792
Renato Verissimo <rverissimo.dev@gmail.com>
2025-10-21 14:08:48 -0300

    fix: normalize JauthServiceApplication to run SpringBoot. Add Flyway and PostgreSQL backends to Java client

--- jauth-service/src/main/java/com/coltran/jauth_service/JauthServiceApplication.java
+++ jauth-service/src/main/java/com/coltran/jauth_service/JauthServiceApplication.java
@@ -1,7 +1,9 @@
 package com.coltran.jauth_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class JauthServiceApplication {
 
 	public static void main(String[] args) {
