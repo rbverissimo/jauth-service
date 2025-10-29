b6882046b2f4fdbba9ad1e7a88f6655ca99b6a40
Renato Verissimo <rverissimo.dev@gmail.com>
2025-10-29 11:18:48 -0300

    fix: downgrading to Spring Boot 3.5.3 and fixing database access

--- jauth-service/src/main/resources/db/migration/V1__create_users_and_roles_tables.sql
+++ jauth-service/src/main/resources/db/migration/V1__create_users_and_roles_tables.sql
@@ -1,12 +1,12 @@
-CREATE EXTENSION IF NOT EXISTS "pg_ulid";
+CREATE EXTENSION IF NOT EXISTS pg_idkit;
 
 CREATE TABLE roles (
-    id ULID PRIMARY KEY DEFAULT ulid(),
+    id CHAR(26) PRIMARY KEY DEFAULT idkit_ulid_generate(),
     name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE users (
-    id ULID PRIMARY KEY DEFAULT ulid(),
+    id CHAR(26) PRIMARY KEY DEFAULT idkit_ulid_generate(),
     email VARCHAR(100) NOT NULL UNIQUE,
     public_name VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -16,8 +16,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE users_roles (
-    user_id ULID NOT NULL,
-    role_id ULID NOT NULL,
+    user_id CHAR(26) NOT NULL,
+    role_id CHAR(26) NOT NULL,
     PRIMARY KEY(user_id, role_id),
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(role_id) REFERENCES roles(id)
