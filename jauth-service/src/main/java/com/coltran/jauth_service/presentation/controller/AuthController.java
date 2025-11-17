a93adc5cd18f1b4427be46d58077f0b7201cb7e3
Renato Verissimo <rverissimo.dev@gmail.com>
2025-11-17 08:53:52 -0300

    feat: add controller to user registration

---
+++ jauth-service/src/main/java/com/coltran/jauth_service/presentation/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.coltran.jauth_service.presentation.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.coltran.jauth_service.application.dto.RegisterRequest;
+import com.coltran.jauth_service.application.dto.UserInfoResponse;
+import com.coltran.jauth_service.application.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService; 
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserInfoResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        UserInfoResponse userInfoResponse = authService.registerLocalUser(registerRequest);
+
+        /* URI location = ServletUriComponentsBuilder
+            .fromCurrentContextPath()
+            .path("/api/users/{id}")
+            .buildAndExpand(userInfoResponse.id()).toUri(); */
+        
+        URI location = null;
+
+        return ResponseEntity.created(location).body(userInfoResponse);
+    }
+    
+}
