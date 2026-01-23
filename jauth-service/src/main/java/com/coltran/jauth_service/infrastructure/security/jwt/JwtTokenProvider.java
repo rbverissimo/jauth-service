c4d74f290bac3044862c3a81ca8c25f7082fb6dd
Renato Verissimo <rverissimo.dev@gmail.com>
2026-01-23 11:34:40 -0300

    feat: adds token generation logic

--- jauth-service/src/main/java/com/coltran/jauth_service/infrastructure/security/jwt/JwtTokenProvider.java
+++ jauth-service/src/main/java/com/coltran/jauth_service/infrastructure/security/jwt/JwtTokenProvider.java
@@ -1,10 +1,22 @@
 package com.coltran.jauth_service.infrastructure.security.jwt;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.coltran.jauth_service.application.port.out.TokenProvider;
 import com.coltran.jauth_service.domain.model.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenProvider implements TokenProvider {
@@ -17,6 +29,23 @@ public class JwtTokenProvider implements TokenProvider {
     
 
     public String generateToken(User user){
-        return null;
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+
+        Instant now = Instant.now();
+        Instant expiryDate = now.plus(jwtExpirationMs, ChronoUnit.MILLIS);
+
+        String roles = user.getRoles()
+            .stream()
+            .map(role -> role.getName().name())
+            .collect(Collectors.joining(","));
+        
+        return Jwts.builder()
+            .subject(user.getId())
+            .claim("email", user.getEmail())
+            .claim("roles", roles)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(expiryDate))
+            .signWith(key)
+            .compact();
     }
 }
