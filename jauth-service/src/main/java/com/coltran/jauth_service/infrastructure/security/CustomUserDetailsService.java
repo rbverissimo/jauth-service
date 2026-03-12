268d332cea282d4d797a4833f76d315e92845208
Renato Verissimo <rverissimo.dev@gmail.com>
2026-03-12 09:28:22 -0300

    feat: adds a CustomUserDetailsService for convenience

---
+++ jauth-service/src/main/java/com/coltran/jauth_service/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.coltran.jauth_service.infrastructure.security;
+
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.coltran.jauth_service.domain.model.User;
+import com.coltran.jauth_service.domain.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+         User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                        .collect(Collectors.toList())
+        );
+    }
+
+    
+}
