3d7d87ba6f89497de5f952940fa79761a9d1e4c4
Renato Verissimo <rverissimo.dev@gmail.com>
2026-03-23 15:24:27 -0300

    feat: adds exception

---
+++ jauth-service/src/main/java/com/coltran/jauth_service/domain/exception/UserException.java
@@ -0,0 +1,12 @@
+package com.coltran.jauth_service.domain.exception;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
