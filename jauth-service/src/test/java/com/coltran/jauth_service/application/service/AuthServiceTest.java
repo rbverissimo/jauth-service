af35aaf46c8fefa98f8b09abad3e7748f19e3d6b
Renato Verissimo <rverissimo.dev@gmail.com>
2026-03-31 15:28:29 -0300

    tests: provides AuthService complete test suite

--- jauth-service/src/test/java/com/coltran/jauth_service/application/service/AuthServiceTest.java
+++ jauth-service/src/test/java/com/coltran/jauth_service/application/service/AuthServiceTest.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.coltran.jauth_service.application.dto.RegisterRequest;
 import com.coltran.jauth_service.application.dto.UserInfoResponse;
+import com.coltran.jauth_service.domain.exception.UserNotFoundException;
 import com.coltran.jauth_service.domain.model.Role;
 import com.coltran.jauth_service.domain.model.RoleName;
 import com.coltran.jauth_service.domain.model.User;
@@ -68,6 +72,83 @@ class AuthServiceTest {
         verify(roleRepository).findByName(RoleName.ROLE_USER.name());
         verify(userRepository).save(any(User.class));
 
+    }
+
+    @Test
+    @DisplayName("Should throw Exception when registering local User with an already existing email ")
+    void registerLocalUser_EmailAlreadyExists_ThrowsExeception(){
+
+        RegisterRequest request = new RegisterRequest("test@mail.com", "testUser123", "password123");
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(true);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            authService.registerLocalUser(request);
+        });
+
+        assertEquals("Error: Email already in use", exception.getMessage());
+
+        verify(passwordEncoder, never()).encode(anyString());
+        verify(roleRepository, never()).findByName(anyString());
+        verify(userRepository, never()).save(any(User.class));
+
+    }
+
+    @Test
+    @DisplayName("Should throw RuntimeException when default ROLE_USER is not found in database")
+    void registerLocalUser_RoleNotFound_ThrowsException() {
+       
+
+        RegisterRequest request = new RegisterRequest(
+            "test@mail.com", 
+            "Test User", 
+            "password123"
+        );
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(false);
+        when(passwordEncoder.encode(request.password())).thenReturn("encodedPassword");
+        when(roleRepository.findByName(RoleName.ROLE_USER.name())).thenReturn(Optional.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            authService.registerLocalUser(request);
+        });
+
+        assertEquals("Error: Default ROLE_USER not found", exception.getMessage());
+
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+
+    @Test
+    @DisplayName("Should successfully retrieve a user by email")
+    void getUserByEmail_Success() {
+
+        String email = "findme@mail.com";
+        User expectedUser = User.createLocalUser(email, "Found User", "hashed");
+        
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(expectedUser));
+
+        User actualUser = authService.getUserByEmail(email);
+
+        assertNotNull(actualUser);
+        assertEquals(email, actualUser.getEmail());
+        verify(userRepository).findByEmail(email);
+    }
+
+    @Test
+    @DisplayName("Should throw UserNotFoundException when retrieving a non-existent email")
+    void getUserByEmail_NotFound_ThrowsException() {
+ 
+        String email = "missing@mail.com";
+        
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            authService.getUserByEmail(email);
+        });
+
+        assertEquals("User not found with email: " + email, exception.getMessage());
+        verify(userRepository).findByEmail(email);
     }
     
 }
